d955f72061028e7d753a5b1975a24257c1a605d6
RambhaSidar <rambhasidar01@gmail.com>
2023-02-10 03:42:35 +0530

    new file

---
+++ src/main/java/com/example/app/bean/AppBean.java
@@ -0,0 +1,23 @@
+package com.example.app.bean;
+
+public class AppBean {
+	private String name;
+	private String value;
+	public AppBean() {
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+
+}
